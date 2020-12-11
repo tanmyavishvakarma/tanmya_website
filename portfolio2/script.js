98a888fe3bf5344a7b3f201030086a1c778e8a41
Tanmya Vishvakarma <tanmya2000@gmail.com>
2020-12-11 12:06:35 +0530

    updated

--- portfolio2/script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $(".a").click(function(){
-        $(this).hide();
-    })
-
-
-
-});
---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $(".a").click(function(){
+        $(this).hide();
+    })
+
+
+
+});
